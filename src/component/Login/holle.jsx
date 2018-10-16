f611391f360133b944cc673cba7d918f7d783254
iamfromsichuan <137548913@qq.com>
2018-10-16 09:30:53 +0800

    增加功能

---
+++ src/component/Login/holle.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { List, InputItem, WhiteSpace } from 'antd';
+class name extends React {
+	constructor(props) {
+	  super(props);
+	}
+	componentWillMount() {
+
+	}
+	render() {
+		return (
+			<div />
+		);
+	}
+}
